511749444d2e97624c328ed31c94f2af9fdcb522
jGraham760 <joe.graham@mymail.champlain.edu>
2024-11-12 23:04:51 -0500

    added scripts

---
+++ TeamProject/Assets/Scripts/CollectablePickUp-1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class CollectablePickUp : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Capitalization and Spelling must be same")]
+    string CollectablePickupTagName = "Collectable";
+
+    private void Start()
+    {
+        GetListOfPickups();
+    }
+
+    private void GetListOfPickups()
+    {
+        try
+        {
+            GameObject[] pickups = GameObject.FindGameObjectsWithTag(CollectablePickupTagName);
+            if (pickups.Length == 0)
+            {
+                Debug.LogWarning("ZERO objects found tagged with " + CollectablePickupTagName);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning("Make sure Tag is created that matche spelling and capitilizatin for Collectable.");
+            Debug.Log(ex.Message);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        try
+        {
+            if (collision.CompareTag(CollectablePickupTagName))
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.LogWarning("Make sure Tag is created that matche spelling and capitilizatin for Collectable.");
+            Debug.Log(ex.Message);
+        }
+    }
+}
